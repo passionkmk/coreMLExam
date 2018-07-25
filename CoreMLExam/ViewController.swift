891b06b600cd0c153c9f55541915c0748322b149
KimMinKu <passion.kmk@gmail.com>
2018-07-25 21:53:45 +0900

    Add UIImagePickerController

--- CoreMLExam/ViewController.swift
+++ CoreMLExam/ViewController.swift
@@ -9,15 +9,17 @@
 import UIKit
 
 // MARK: - Overrides
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var predictLabel: UILabel!
     
+    let imagePicker: UIImagePickerController = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.imagePicker.delegate = self
+        self.predictLabel.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,16 +31,38 @@ class ViewController: UIViewController {
 // MARK: - Actions
 extension ViewController {
     @IBAction func openCamera(_ sender: UIBarButtonItem) {
-        
+        self.showCamera()
     }
     
     @IBAction func openAlbum(_ sender: UIBarButtonItem) {
-        
+        self.showAlbum()
     }
 }
 
 // MARK: - Functions
 extension ViewController {
+    func showCamera() {
+        self.imagePicker.sourceType = .camera
+        self.imagePicker.allowsEditing = true
+        self.present(self.imagePicker, animated: true, completion: nil)
+    }
     
+    func showAlbum() {
+        self.imagePicker.sourceType = .photoLibrary
+        self.present(self.imagePicker, animated: true, completion: nil)
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            self.predictLabel.text = "Not Image Data!"
+            self.dismiss(animated: true, completion: nil)
+            return
+        }
+        self.imageView.image = image
+        // TODO: - set Image Label
+        self.dismiss(animated: true, completion: nil)
+    }
 }
 
