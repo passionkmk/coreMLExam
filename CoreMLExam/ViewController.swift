64afda5aac254bb1a7f8b5f286646bc35c9f5ee6
KimMinKu <passion.kmk@gmail.com>
2018-07-25 20:14:53 +0900

    Initial Commit

---
+++ CoreMLExam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreMLExam
+//
+//  Created by KimMinKu on 2018. 7. 25..
+//  Copyright © 2018년 KimMinKu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
